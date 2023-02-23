cb42076a32443f8f22fc02aa6111b791b6a47c4f
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-02-23 12:09:46 +0100

    completed assignemnt

--- app/controllers/productcontroller.php
+++ app/controllers/productcontroller.php
@@ -18,6 +18,13 @@ class ProductController extends Controller
     public function getAll()
     {
         // this code seems to have been lost
+        $products = $this->service->getAll();
+        $this->respond($products);
+
+    }
+
+    public function delete($id){
+        $this->service->delete($id);
     }
 
     public function getOne($id)
@@ -25,6 +32,11 @@ class ProductController extends Controller
         $product = $this->service->getOne($id);
 
         // we might need some kind of error checking that returns a 404 if the product is not found in the DB
+
+        if ($product == null) {
+            $this->respondWithError(404, "Product not found");
+            return;
+        }
 
         $this->respond($product);
     }
@@ -34,6 +46,7 @@ class ProductController extends Controller
         try {
             $product = $this->createObjectFromPostedJson("Models\Product");
             // something is missing. Shouldn't we update the product in the DB?
+            $this->service->insert($product);
 
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
@@ -44,6 +57,22 @@ class ProductController extends Controller
 
     public function update($id)
     {
-        // There is no code here
+
+        // does the product exist?
+        $product = $this->service->getOne($id);
+
+        if ($product == null) {
+            $this->respondWithError(404, "Product not found");
+            return;
+        }
+
+        // update the product
+        $product = $this->createObjectFromPostedJson("Models\Product");
+        $this->service->update($product, $id);
+
+        // display the updated product
+        $this->service->getOne($id);
+        $this->respond($product);
+
     } 
 }
